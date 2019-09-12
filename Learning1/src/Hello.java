15fa08d3e34df3c9acc1e66babcbe4c91cbdfd30
abromma <aaron.bromma@latitudetech.com>
2019-09-12 09:57:59 -0700

    Added Learning 1 project

---
+++ Learning1/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		int test = 99;
+		
+		System.out.println("The variable is " + test);
+	}
+
+}
