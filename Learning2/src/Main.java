f29d468e67d66cfe82c94dbc9e252f25d0bb1fe9
bioresestance <aaron.bromma@gmail.com>
2019-09-12 10:55:09 -0700

    Playing Around with Sytax.

---
+++ Learning2/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Just playing around and verifying that Java uses C/C++ like syntax.
+		
+		char Character = 0x0031;
+		System.out.println(Character);
+		
+		if(Character == '0') {
+			System.out.println("Yup");
+		} else {
+			System.out.println("Nope");
+		}
+		
+		boolean test = (Character == '0') ? true : false;
+		
+		if(test) {
+			System.out.println("Yup again");
+		} else {
+			System.out.println("Nope Again");
+		}
+	}
+
+}
